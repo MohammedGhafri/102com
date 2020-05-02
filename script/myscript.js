84a42511aa784fb2e3971b1bf53ef05ef6ac4b6e
MohammedGhafri <eng.m.ghafri@gamil.com>
2020-05-02 21:04:12 +0300

    lab 6

--- script/myscript.js
+++ script/myscript.js
@@ -1,13 +1,16 @@
 'use strict';
 //this is my first java scripts
+function numOne(){
 
 var Time = new Date();
 
 var first= confirm('In the next Window please enter your favorite Engineering(Just Enter First letter)_press ok thing to continue');
 
 var confirmation = confirm (' after you choosed the flavoured engineering go to the bottom of the page to see short definition')
-var list;
+var list = 0;
 
+var name= prompt('enter Your name');
+var age = prompt(' Enter Your Age');
 var Falvoured_Engineering = prompt('Electrical Engineering = e Civil Engineering = c mechanical Engineering = m ');
 
 if (Falvoured_Engineering == 'e') {
@@ -19,8 +22,29 @@ list ='Civil engineering is a professional engineering discipline that deals wit
 }else{
     list=' Either you do not like engineering or you did not enter well'
 }
-document.write('<h2>' +list+ '</h2>');
+document.write('<pe class="time">'+Time+'</pe>');
 
+document.write('<pre class ="name">'+ '1)Your name  is :  '+''+ name + '</pre>');
+document.write('<pre class ="age">'+ '2)Your Age is   :  '+ age + '</pre>');
+document.write('<pre class ="fav">'+ '3)Favourit Engineering is (Note that this is changeable upon your chioce):  ' + '</pre>');
 
+return list;
+}
 
-
+function calculate()
+{
+    var userInput= prompt('enter your value');
+}
+function myFunction() {
+    var tt = new Date();
+    var n = tt.getHours();
+    alert(tt);
+    if (n>18 ){
+        alert('good Evening')
+    }else if(n>12){
+        alert('Good AFter noon')
+    }else if(n>0){
+        alert('Good morning')
+    }
+    
+}
